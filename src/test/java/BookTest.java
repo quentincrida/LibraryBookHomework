e36a383543003ec568aeb3ffae316d7df98725b2
Quentin Crida <qcrida@gmail.com>
2020-01-28 17:25:47 +0000

    tests for book name and author

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void before() {
+        book = new Book("Hell", "Stephen King", "Horror");
+    }
+    @Test
+    public void bookHasTitle(){
+        assertEquals("Hell", book.getTitle());
+    }
+    @Test
+    public void bookHasAuthor(){
+        assertEquals("Stephen King", book.getAuthor());
+    }
 }
