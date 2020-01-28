4c66cc23c7818dfb0c48ea3b1d2dc4778f3bdd3e
Quentin Crida <qcrida@gmail.com>
2020-01-28 18:48:46 +0000

    can add book tested

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> stock;
+
+    public Library() {
+        this.stock = new ArrayList<Book>();
+    }
+
+    public int stockCount() {
+        return this.stock.size();
+    }
+
+    public void add(Book book) {
+        this.stock.add(book);
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class LibraryTest {
+
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before() {
+        library = new Library();
+        book = new Book("Hell", "Stephen King", "Horror");
+    }
+
+   @Test
+    public void canCheckStock(){
+        assertEquals(0, library.stockCount());
+    }
+    @Test
+    public void canAddBookToLibrary(){
+        library.add(book);
+        assertEquals(1, library.stockCount() );
+    }
 }
+
